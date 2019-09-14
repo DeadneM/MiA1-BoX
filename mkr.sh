7c03c6138764dd0f815104b336310ae8a6a37fec
DeadneM <deadnem@gmail.com>
2019-09-14 16:43:34 +0200

    Make zip deleted when zipping a new one

--- mkr.sh
+++ mkr.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 cd /home/guest/Documents/MiA1BoX_ModuleMaker
 
-if [ ! -d MiA1BoX ]; then
-echo "No Module Directory Found"
-else
+if [ ! -d Releases/ ] | [ $(grep "MiA1") ]; then
 echo "Zipping Module files"
+else
+echo "Module zip already there, so deleting the old ones"
+rm -f Releases/*.zip
+fi
 
 cd MiA1BoX
 zip -9 -r ../MiA1BoX.zip .
@@ -12,6 +14,6 @@ mv ../MiA1BoX.zip /home/guest/Documents/MiA1BoX_ModuleMaker/Releases/MiA1BoX-$( 
 cd ../..
 
 echo "Finished Zipping."
-fi
+
 exit
 
