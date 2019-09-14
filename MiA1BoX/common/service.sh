b9c09bd19a6561151c0616f798ae17c219117188
DeadneM <deadnem@gmail.com>
2019-09-14 03:38:05 +0200

    add some settings put values for some things related to lockscreen and misc

--- MiA1BoX/common/service.sh
+++ MiA1BoX/common/service.sh
@@ -8,10 +8,22 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 
-#This will force gpu composition to avoid flickering
-while true; do BOOT=$(getprop sys.boot_completed); if [ "$BOOT" -eq "1" ]; then sleep 9; break; else sleep 3; fi; done
+#Change lockscreen shortcut
+su -c settings put secure sysui_keyguard_left=org.thunderdog.challegram/org.thunderdog.challegram.MainActivity
+su -c settings put secure sysui_keyguard_right=com.snapchat.android/com.snap.mushroom.MainActivity
+#Other misc settings changes
+# su -c settings put global stay_on_while_plugged_in 0
+su -c settings put global boot_count 8
+su -c settings put global Phenotype_boot_count 8
+#screen_off_timeout=120000; notification_light_pulse=1; notification_vibration_intensity=2; haptic_feedback_enabled=1; haptic_feedback_intensity=2
+su -c settings put secure clock_seconds 1
+su -c settings put system status_bar_show_battery_percent 1
+su -c settings put secure icon_blacklist rotate
 
-while true; do SUFI=$(service list | grep -c "SurfaceFlinger"); if [ $SUFI -eq "1" ]; then sleep 9; break; else sleep 3; fi; done
+#This will force gpu composition to avoid flickering
+while true; do BOOT=$(getprop sys.boot_completed); if [ "$BOOT" -eq "1" ]; then sleep 12; break; else sleep 6; fi; done
+
+while true; do SUFI=$(service list | grep -c "SurfaceFlinger"); if [ $SUFI -eq "1" ]; then sleep 12; break; else sleep 6; fi; done
 
 su -c service call SurfaceFlinger 1008 i64 1
 
