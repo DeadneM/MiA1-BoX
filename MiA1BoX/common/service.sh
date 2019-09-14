4e1cc63d13af2457a5099aab0d2c495ba731e8a8
DeadneM <deadnem@gmail.com>
2019-09-14 18:32:45 +0200

    Perfect delay for lockscreen shortcut ?

--- MiA1BoX/common/service.sh
+++ MiA1BoX/common/service.sh
@@ -9,11 +9,16 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 
 #WAIT TILL BOOT IS COMPLETE
-while true; do BOOT=$(getprop sys.boot_completed); if [ "$BOOT" -eq "1" ]; then sleep 12; break; else sleep 6; fi; done
+while true; do BOOT=$(getprop sys.boot_completed); if [ "$BOOT" -eq "1" ]; then sleep 3; break; else sleep 6; fi; done
 
 #Disable Commentary and spock app for MiA1
 su -c pm disable com.miui.spock
 su -c pm disable com.miui.bugreport
+
+#This will force gpu composition to avoid flickering
+while true; do SUFI=$(service list | grep -c "SurfaceFlinger"); if [ $SUFI -eq "1" ]; then sleep 12; break; else sleep 9; fi; done
+# su -c service call SurfaceFlinger 1008 i32 1
+su -c service call SurfaceFlinger 1008 i64 1
 
 #Change lockscreen shortcut
 su -c settings put secure sysui_keyguard_left org.thunderdog.challegram/org.thunderdog.challegram.MainActivity
@@ -26,9 +31,4 @@ su -c settings put global Phenotype_boot_count 8
 su -c settings put secure clock_seconds 1
 su -c settings put system status_bar_show_battery_percent 1
 su -c settings put secure icon_blacklist rotate
-
-#This will force gpu composition to avoid flickering
-while true; do SUFI=$(service list | grep -c "SurfaceFlinger"); if [ $SUFI -eq "1" ]; then sleep 12; break; else sleep 6; fi; done
-# su -c service call SurfaceFlinger 1008 i32 1
-su -c service call SurfaceFlinger 1008 i64 1
 
