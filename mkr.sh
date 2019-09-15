1ed36232c287cfd3fd21b3c7571e66bde4e9ec31
DeadneM <deadnem@gmail.com>
2019-09-15 22:51:16 +0200

    mkr.sh have now a menu debug mode for zip verbose stuff and choose for different module that need to be added manualy in mkr.sh i have just added n6 module. thanks to john fawkes font changer magisk module this is where i looked to know how menu in bash work

--- mkr.sh
+++ mkr.sh
@@ -1,19 +1,85 @@
 #!/bin/bash
-cd /home/guest/Documents/MiA1BoX_ModuleMaker
+#Zip log
+ZIPLOG=0
+#Path to root of git repo # Need to match current root of module maker path
+PATHGIT=/home/guest/Documents/MiA1BoX_ModuleMaker
+#Name of module folder # you can add MOD(NUMBER) path
+MOD1=MiA1BoX
+MOD2=N6BoX
+#Name of module that is zipped # should stay null
+MODULEZIP=NULL
 
-if [ ! -d Releases/ ] | [ $(grep "MiA1") ]; then
-echo "Zipping Module files"
+####################---Menu---#######################START
+menu() {
+    echo "Place your module here $PATHGIT"; echo "----------------------"; echo "-Magisk Module Zipper-"; echo "----------------------"
+#Switch debug zip
+if [ $ZIPLOG -eq "1" ]; then echo "Current debug switch is on"; else if [ $ZIPLOG -eq "0" ]; then echo "Current debug switch is off"; fi fi
+        echo "Type 00 to switch debug zip"
+#Exit
+        echo "Type 0 to exit"
+#MOD1
+        echo "Type 1 for $MOD1"
+#MOD2
+        echo "Type 2 for $MOD2"
+###IF YOU ADD MODULE TO ZIP ADD ENTRY HERE AND BELOW
+read -r choice
+    case $choice in
+    0)
+        echo "---Exit---"
+        exit
+    ;;
+    00)
+        echo "---Switch Debug Zip---"
+            if [ $ZIPLOG -eq "1" ]; then
+                ZIPLOG=0
+                echo "Zip debug switch is off"
+            else
+                ZIPLOG=1
+                echo "Zip debug switch is on"
+            fi
+        menu
+    ;;
+    1)
+        echo "---$MOD1 module selected---"
+        MODULEZIP=$MOD1
+        Zipping
+    ;;
+    2)
+        echo "---$MOD2 module selected---"
+        MODULEZIP=$MOD2
+        Zipping
+    ;;
+###IF YOU ADD MODULE TO ZIP ADD ENTRY HERE AND BELOW
+esac
+}
+####################---Menu---#######################END
+
+####################---ZIP-FUNCTION---#######################START
+Zipping() {
+cd $PATHGIT
+if [ ! -d Releases/ ] | [ $(grep "$MODULEZIP") ]; then
+echo "Zipping $MODULEZIP Module files"
 else
-echo "Module zip already there, so deleting the old ones"
-rm -f Releases/*.zip
+echo "$MODULEZIP Module zip already there, so deleting the old ones"
+rm -f Releases/$MODULEZIP*.zip
 fi
-
-cd MiA1BoX
-zip -9 -r ../MiA1BoX.zip .
-mv ../MiA1BoX.zip /home/guest/Documents/MiA1BoX_ModuleMaker/Releases/MiA1BoX-$( date +"%d%m%y-%H%M%S" ).zip
+cd $MODULEZIP
+echo "Starting Zipping..."
+if [ $ZIPLOG -eq "1" ]; then
+zip -9 -r -v -T ../$MODULEZIP.zip .
+else if [ $ZIPLOG -eq "0" ]; then
+zip -9 -r -q -T ../$MODULEZIP.zip .
+fi
+fi
+echo "Finished Zipping..."
+mv ../$MODULEZIP.zip $PATHGIT/Releases/$MODULEZIP-$( date +"%d%m%y-%H%M%S" ).zip
+echo "Moving $MODULEZIP.zip into releases folder"
 cd ../..
-
-echo "Finished Zipping."
-
+echo "Finished $MODULEZIP module zipping"
+echo "Look for $MODULEZIP into $PATHGIT/Releases"
 exit
+}
+####################---ZIP-FUNCTION---#######################END
+
+menu
 
