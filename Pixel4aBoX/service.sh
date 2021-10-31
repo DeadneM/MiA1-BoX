1ad31ddd60e2664597a07816918a939954c40f28
DeadneM <deadnem@gmail.com>
2021-10-31 17:34:45 +0100

    Make it so simple that its now a simple mod

--- Pixel4aBoX/service.sh
+++ Pixel4aBoX/service.sh
@@ -11,16 +11,56 @@ MODDIR=${0%/*}
 # # # WAIT TILL BOOT IS COMPLETE # # #
 while true; do BOOT=$(getprop sys.boot_completed); if [ "$BOOT" -eq "1" ]; then sleep 3; break; else sleep 6; fi; done
 
+# # # SCHED TUNING # # #
+echo "0" > /proc/sys/kernel/sched_schedstats
+echo "1" > /proc/sys/kernel/sched_tunable_scaling
+#general
+echo "0" > /dev/stune/schedtune.boost
+echo "0" > /dev/stune/schedtune.prefer_high_cap
+echo "1" > /dev/stune/schedtune.prefer_idle
+#background
+echo "0" > /dev/stune/background/schedtune.boost
+echo "0" > /dev/stune/background/schedtune.prefer_high_cap
+echo "1" > /dev/stune/background/schedtune.prefer_idle
+echo "0-5" > /dev/cpuset/background/cpus
+#foreground
+echo "0" > /dev/stune/foreground/schedtune.boost
+echo "0" > /dev/stune/foreground/schedtune.prefer_high_cap
+echo "1" > /dev/stune/foreground/schedtune.prefer_idle
+echo "0-5,7" > /dev/cpuset/foreground/cpus
+#top-app
+echo "0" > /dev/stune/top-app/schedtune.boost
+echo "0" > /dev/stune/top-app/schedtune.prefer_high_cap
+echo "1" > /dev/stune/top-app/schedtune.prefer_idle
+echo "0-5,7" > /dev/cpuset/top-app/cpus
+#camera-daemon
+echo "0" > /dev/stune/camera-daemon/schedtune.boost
+echo "0" > /dev/stune/camera-daemon/schedtune.prefer_high_cap
+echo "1" > /dev/stune/camera-daemon/schedtune.prefer_idle
+echo "0-7" > /dev/cpuset/camera-daemon/cpus
+#nnapi-hal
+echo "0" > /dev/stune/nnapi-hal/schedtune.boost
+echo "0" > /dev/stune/nnapi-hal/schedtune.prefer_high_cap
+echo "1" > /dev/stune/nnapi-hal/schedtune.prefer_idle
+#rt
+echo "0" > /dev/stune/rt/schedtune.boost
+echo "0" > /dev/stune/rt/schedtune.prefer_high_cap
+echo "1" > /dev/stune/rt/schedtune.prefer_idle
+#restricted
+echo "0-2,5" > /dev/cpuset/restricted/cpus
+#system background
+echo "0-2,5" > /dev/cpuset/system-background/cpus
+
 #Disable Left and Right back gestures
 settings put secure back_gesture_inset_scale_left -1 && settings put secure back_gesture_inset_scale_right -1
 #Restore back gesture on uninstallation
 # settings delete secure back_gesture_inset_scale_left && settings delete secure back_gesture_inset_scale_right
-#Reset the boot count for fun
-settings put global boot_count 8
-settings put global Phenotype_boot_count 8
-settings put secure low_power_manual_activation_count 8
 #Blacklist only rotation icon
 settings put secure icon_blacklist rotate
 #Show low priority icons
 settings put secure low_priority 1
+#Reset the boot count for fun
+settings put global boot_count 8
+settings put global Phenotype_boot_count 8
+settings put secure low_power_manual_activation_count 8
 
