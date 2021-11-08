083a39e825beea8d8cdae74d43d3495468b76df7
DeadneM <deadnem@gmail.com>
2021-11-08 20:32:08 +0100

    Pixel4a update to speed up boot and do some cpu things

--- Pixel4aBoX/service.sh
+++ Pixel4aBoX/service.sh
@@ -8,12 +8,13 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 
-# # # WAIT TILL BOOT IS COMPLETE # # #
-while true; do BOOT=$(getprop sys.boot_completed); if [ "$BOOT" -eq "1" ]; then sleep 3; break; else sleep 6; fi; done
-
-# # # SCHED TUNING # # #
+# # # KERNEL SCHED # # #
 echo "0" > /proc/sys/kernel/sched_schedstats
 echo "1" > /proc/sys/kernel/sched_tunable_scaling
+# # # SCHEDUTIL FREQ # # #
+echo "1248000" > /sys/devices/system/cpu/cpufreq/policy0/schedutil/highspeedfreq
+echo "1555200" > /sys/devices/system/cpu/cpufreq/policy6/schedutil/highspeedfreq
+# # # SCHEDTUNE # # #
 #general
 echo "0" > /dev/stune/schedtune.boost
 echo "0" > /dev/stune/schedtune.prefer_high_cap
@@ -22,22 +23,22 @@ echo "1" > /dev/stune/schedtune.prefer_idle
 echo "0" > /dev/stune/background/schedtune.boost
 echo "0" > /dev/stune/background/schedtune.prefer_high_cap
 echo "1" > /dev/stune/background/schedtune.prefer_idle
-echo "0-5" > /dev/cpuset/background/cpus
+echo "0-5,7" > /dev/cpuset/background/cpus
 #foreground
 echo "0" > /dev/stune/foreground/schedtune.boost
 echo "0" > /dev/stune/foreground/schedtune.prefer_high_cap
 echo "1" > /dev/stune/foreground/schedtune.prefer_idle
 echo "0-5,7" > /dev/cpuset/foreground/cpus
 #top-app
-echo "0" > /dev/stune/top-app/schedtune.boost
-echo "0" > /dev/stune/top-app/schedtune.prefer_high_cap
-echo "1" > /dev/stune/top-app/schedtune.prefer_idle
+echo "0" > /dev/stune/top-app/schedtune.boost && chmod 0444 /dev/stune/top-app/schedtune.boost
+echo "0" > /dev/stune/top-app/schedtune.prefer_high_cap && chmod 0444 /dev/stune/top-app/schedtune.prefer_high_cap
+echo "1" > /dev/stune/top-app/schedtune.prefer_idle && chmod 0444 /dev/stune/top-app/schedtune.prefer_idle
 echo "0-5,7" > /dev/cpuset/top-app/cpus
 #camera-daemon
 echo "0" > /dev/stune/camera-daemon/schedtune.boost
 echo "0" > /dev/stune/camera-daemon/schedtune.prefer_high_cap
 echo "1" > /dev/stune/camera-daemon/schedtune.prefer_idle
-echo "0-7" > /dev/cpuset/camera-daemon/cpus
+echo "0-5,7" > /dev/cpuset/camera-daemon/cpus
 #nnapi-hal
 echo "0" > /dev/stune/nnapi-hal/schedtune.boost
 echo "0" > /dev/stune/nnapi-hal/schedtune.prefer_high_cap
@@ -50,10 +51,12 @@ echo "1" > /dev/stune/rt/schedtune.prefer_idle
 echo "0-2,5" > /dev/cpuset/restricted/cpus
 #system background
 echo "0-2,5" > /dev/cpuset/system-background/cpus
-
+# # # WAIT TILL BOOT IS COMPLETE # # #
+while true; do BOOT=$(getprop sys.boot_completed); if [ "$BOOT" -eq "1" ]; then sleep 3; break; else sleep 6; fi; done
+# # # CUSTOM SYSTEM UI SETTINGS # # #
 #Disable Left and Right back gestures
 settings put secure back_gesture_inset_scale_left -1 && settings put secure back_gesture_inset_scale_right -1
-#Restore back gesture on uninstallation
+#Restore Left and Right back gestures
 # settings delete secure back_gesture_inset_scale_left && settings delete secure back_gesture_inset_scale_right
 #Blacklist only rotation icon
 settings put secure icon_blacklist rotate
