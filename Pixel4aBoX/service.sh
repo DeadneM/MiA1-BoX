ff0a391328f7cd78c91e3924f8b63abd708f4ad6
DeadneM <deadnem@gmail.com>
2021-11-12 20:25:49 +0100

    chmod 0444 every kernel tunables to make sure they dont change after some time

--- Pixel4aBoX/service.sh
+++ Pixel4aBoX/service.sh
@@ -9,50 +9,50 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 
 # # # KERNEL SCHED # # #
-echo "0" > /proc/sys/kernel/sched_schedstats
-echo "1" > /proc/sys/kernel/sched_tunable_scaling
+echo '0' > /proc/sys/kernel/sched_schedstats && chmod 0444 /proc/sys/kernel/sched_schedstats
+echo '1' > /proc/sys/kernel/sched_tunable_scaling && chmod 0444 /proc/sys/kernel/sched_tunable_scaling
 # # # SCHEDUTIL FREQ # # #
-echo "1248000" > /sys/devices/system/cpu/cpufreq/policy0/schedutil/highspeedfreq
-echo "1555200" > /sys/devices/system/cpu/cpufreq/policy6/schedutil/highspeedfreq
+echo '1248000' > /sys/devices/system/cpu/cpufreq/policy0/schedutil/highspeedfreq && chmod 0444 /sys/devices/system/cpu/cpufreq/policy0/schedutil/highspeedfreq
+echo '1555200' > /sys/devices/system/cpu/cpufreq/policy6/schedutil/highspeedfreq && chmod 0444 /sys/devices/system/cpu/cpufreq/policy6/schedutil/highspeedfreq
 # # # SCHEDTUNE # # #
 #general
-echo "0" > /dev/stune/schedtune.boost
-echo "0" > /dev/stune/schedtune.prefer_high_cap
-echo "1" > /dev/stune/schedtune.prefer_idle
+echo '0' > /dev/stune/schedtune.boost && chmod 0444 /dev/stune/schedtune.boost
+echo '0' > /dev/stune/schedtune.prefer_high_cap && chmod 0444 /dev/stune/schedtune.prefer_high_cap
+echo '1' > /dev/stune/schedtune.prefer_idle && chmod 0444 /dev/stune/schedtune.prefer_idle
 #background
-echo "0" > /dev/stune/background/schedtune.boost
-echo "0" > /dev/stune/background/schedtune.prefer_high_cap
-echo "1" > /dev/stune/background/schedtune.prefer_idle
-echo "0-5,7" > /dev/cpuset/background/cpus
+echo '0' > /dev/stune/background/schedtune.boost && chmod 0444 /dev/stune/background/schedtune.boost
+echo '0' > /dev/stune/background/schedtune.prefer_high_cap && chmod 0444 /dev/stune/background/schedtune.prefer_high_cap
+echo '1' > /dev/stune/background/schedtune.prefer_idle && chmod 0444 /dev/stune/background/schedtune.prefer_idle
+echo '0-5,7' > /dev/cpuset/background/cpus && chmod 0444 /dev/cpuset/background/cpus
 #foreground
-echo "0" > /dev/stune/foreground/schedtune.boost
-echo "0" > /dev/stune/foreground/schedtune.prefer_high_cap
-echo "1" > /dev/stune/foreground/schedtune.prefer_idle
-echo "0-5,7" > /dev/cpuset/foreground/cpus
+echo '0' > /dev/stune/foreground/schedtune.boost && chmod 0444 /dev/stune/foreground/schedtune.boost
+echo '0' > /dev/stune/foreground/schedtune.prefer_high_cap && chmod 0444 /dev/stune/foreground/schedtune.prefer_high_cap
+echo '1' > /dev/stune/foreground/schedtune.prefer_idle && chmod 0444 /dev/stune/foreground/schedtune.prefer_idle
+echo '0-5,7' > /dev/cpuset/foreground/cpus && chmod 0444 /dev/cpuset/foreground/cpus
 #top-app
-echo "0" > /dev/stune/top-app/schedtune.boost && chmod 0444 /dev/stune/top-app/schedtune.boost
-echo "0" > /dev/stune/top-app/schedtune.prefer_high_cap && chmod 0444 /dev/stune/top-app/schedtune.prefer_high_cap
-echo "1" > /dev/stune/top-app/schedtune.prefer_idle && chmod 0444 /dev/stune/top-app/schedtune.prefer_idle
-echo "0-5,7" > /dev/cpuset/top-app/cpus
+echo '0' > /dev/stune/top-app/schedtune.boost && chmod 0444 /dev/stune/top-app/schedtune.boost
+echo '0' > /dev/stune/top-app/schedtune.prefer_high_cap && chmod 0444 /dev/stune/top-app/schedtune.prefer_high_cap
+echo '1' > /dev/stune/top-app/schedtune.prefer_idle && chmod 0444 /dev/stune/top-app/schedtune.prefer_idle
+echo '0-5,7' > /dev/cpuset/top-app/cpus && chmod 0444 /dev/cpuset/top-app/cpus
 #camera-daemon
-echo "0" > /dev/stune/camera-daemon/schedtune.boost
-echo "0" > /dev/stune/camera-daemon/schedtune.prefer_high_cap
-echo "1" > /dev/stune/camera-daemon/schedtune.prefer_idle
-echo "0-5,7" > /dev/cpuset/camera-daemon/cpus
+echo '0' > /dev/stune/camera-daemon/schedtune.boost && chmod 0444 /dev/stune/camera-daemon/schedtune.boost
+echo '0' > /dev/stune/camera-daemon/schedtune.prefer_high_cap && chmod 0444 /dev/stune/camera-daemon/schedtune.prefer_high_cap
+echo '1' > /dev/stune/camera-daemon/schedtune.prefer_idle && chmod 0444 /dev/stune/camera-daemon/schedtune.prefer_idle
+echo '0-5,7' > /dev/cpuset/camera-daemon/cpus && chmod 0444 /dev/cpuset/camera-daemon/cpus
 #nnapi-hal
-echo "0" > /dev/stune/nnapi-hal/schedtune.boost
-echo "0" > /dev/stune/nnapi-hal/schedtune.prefer_high_cap
-echo "1" > /dev/stune/nnapi-hal/schedtune.prefer_idle
+echo '0' > /dev/stune/nnapi-hal/schedtune.boost && chmod 0444 /dev/stune/nnapi-hal/schedtune.boost
+echo '0' > /dev/stune/nnapi-hal/schedtune.prefer_high_cap && chmod 0444 /dev/stune/nnapi-hal/schedtune.prefer_high_cap
+echo '1' > /dev/stune/nnapi-hal/schedtune.prefer_idle && chmod 0444 /dev/stune/nnapi-hal/schedtune.prefer_idle
 #rt
-echo "0" > /dev/stune/rt/schedtune.boost
-echo "0" > /dev/stune/rt/schedtune.prefer_high_cap
-echo "1" > /dev/stune/rt/schedtune.prefer_idle
+echo '0' > /dev/stune/rt/schedtune.boost && chmod 0444 /dev/stune/rt/schedtune.boost
+echo '0' > /dev/stune/rt/schedtune.prefer_high_cap && chmod 0444 /dev/stune/rt/schedtune.prefer_high_cap
+echo '1' > /dev/stune/rt/schedtune.prefer_idle && chmod 0444 /dev/stune/rt/schedtune.prefer_idle
 #restricted
-echo "0-2,5" > /dev/cpuset/restricted/cpus
+echo '0-2,5' > /dev/cpuset/restricted/cpus && chmod 0444 /dev/cpuset/restricted/cpus
 #system background
-echo "0-2,5" > /dev/cpuset/system-background/cpus
+echo '0-2,5' > /dev/cpuset/system-background/cpus && chmod 0444 /dev/cpuset/system-background/cpus
 # # # WAIT TILL BOOT IS COMPLETE # # #
-while true; do BOOT=$(getprop sys.boot_completed); if [ "$BOOT" -eq "1" ]; then sleep 3; break; else sleep 6; fi; done
+while true; do BOOT=$(getprop sys.boot_completed); if [ '$BOOT' -eq '1' ]; then sleep 3; break; else sleep 9; fi; done
 # # # CUSTOM SYSTEM UI SETTINGS # # #
 #Disable Left and Right back gestures
 settings put secure back_gesture_inset_scale_left -1 && settings put secure back_gesture_inset_scale_right -1
