b617e775f2f752813f11dc04a3b7beeb7075cdbb
DeadneM <deadnem@gmail.com>
2021-11-29 12:29:34 +0100

    V11 pixel change some behavior for idle and highperf

--- Pixel4aBoX/service.sh
+++ Pixel4aBoX/service.sh
@@ -11,11 +11,13 @@ echo '1248000' > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq;
 echo '1555200' > /sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_freq; chmod 0444 /sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_freq
 echo '95' > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_load; chmod 0444 /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_load
 echo '95' > /sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_load; chmod 0444 /sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_load
+echo '1' > /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl; chmod 0444 /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl
+echo '1' > /sys/devices/system/cpu/cpufreq/policy6/schedutil/pl; chmod 0444 /sys/devices/system/cpu/cpufreq/policy6/schedutil/pl
 # # # SCHEDTUNE # # #
 #general
 echo '0' > /dev/stune/schedtune.boost; chmod 0444 /dev/stune/schedtune.boost
 echo '0' > /dev/stune/schedtune.prefer_high_cap; chmod 0444 /dev/stune/schedtune.prefer_high_cap
-echo '1' > /dev/stune/schedtune.prefer_idle; chmod 0444 /dev/stune/schedtune.prefer_idle
+echo '0' > /dev/stune/schedtune.prefer_idle; chmod 0444 /dev/stune/schedtune.prefer_idle
 echo '0-7' > /dev/cpuset/cpus; chmod 0444 /dev/cpuset/cpus
 #background
 echo '0' > /dev/stune/background/schedtune.boost; chmod 0444 /dev/stune/background/schedtune.boost
@@ -24,13 +26,13 @@ echo '1' > /dev/stune/background/schedtune.prefer_idle; chmod 0444 /dev/stune/ba
 echo '0-2,5' > /dev/cpuset/background/cpus; chmod 0444 /dev/cpuset/background/cpus
 #foreground
 echo '0' > /dev/stune/foreground/schedtune.boost; chmod 0444 /dev/stune/foreground/schedtune.boost
-echo '0' > /dev/stune/foreground/schedtune.prefer_high_cap; chmod 0444 /dev/stune/foreground/schedtune.prefer_high_cap
-echo '1' > /dev/stune/foreground/schedtune.prefer_idle; chmod 0444 /dev/stune/foreground/schedtune.prefer_idle
+echo '1' > /dev/stune/foreground/schedtune.prefer_high_cap; chmod 0444 /dev/stune/foreground/schedtune.prefer_high_cap
+echo '0' > /dev/stune/foreground/schedtune.prefer_idle; chmod 0444 /dev/stune/foreground/schedtune.prefer_idle
 echo '0-5,7' > /dev/cpuset/foreground/cpus; chmod 0444 /dev/cpuset/foreground/cpus
 #top-app
 echo '0' > /dev/stune/top-app/schedtune.boost; chmod 0444 /dev/stune/top-app/schedtune.boost
-echo '0' > /dev/stune/top-app/schedtune.prefer_high_cap; chmod 0444 /dev/stune/top-app/schedtune.prefer_high_cap
-echo '1' > /dev/stune/top-app/schedtune.prefer_idle; chmod 0444 /dev/stune/top-app/schedtune.prefer_idle
+echo '1' > /dev/stune/top-app/schedtune.prefer_high_cap; chmod 0444 /dev/stune/top-app/schedtune.prefer_high_cap
+echo '0' > /dev/stune/top-app/schedtune.prefer_idle; chmod 0444 /dev/stune/top-app/schedtune.prefer_idle
 echo '0-5,7' > /dev/cpuset/top-app/cpus; chmod 0444 /dev/cpuset/top-app/cpus
 #camera-daemon
 echo '0' > /dev/stune/camera-daemon/schedtune.boost; chmod 0444 /dev/stune/camera-daemon/schedtune.boost
@@ -40,11 +42,11 @@ echo '0-5,7' > /dev/cpuset/camera-daemon/cpus; chmod 0444 /dev/cpuset/camera-dae
 #nnapi-hal
 echo '0' > /dev/stune/nnapi-hal/schedtune.boost; chmod 0444 /dev/stune/nnapi-hal/schedtune.boost
 echo '0' > /dev/stune/nnapi-hal/schedtune.prefer_high_cap; chmod 0444 /dev/stune/nnapi-hal/schedtune.prefer_high_cap
-echo '1' > /dev/stune/nnapi-hal/schedtune.prefer_idle; chmod 0444 /dev/stune/nnapi-hal/schedtune.prefer_idle
+echo '0' > /dev/stune/nnapi-hal/schedtune.prefer_idle; chmod 0444 /dev/stune/nnapi-hal/schedtune.prefer_idle
 #rt
 echo '0' > /dev/stune/rt/schedtune.boost; chmod 0444 /dev/stune/rt/schedtune.boost
 echo '0' > /dev/stune/rt/schedtune.prefer_high_cap; chmod 0444 /dev/stune/rt/schedtune.prefer_high_cap
-echo '1' > /dev/stune/rt/schedtune.prefer_idle; chmod 0444 /dev/stune/rt/schedtune.prefer_idle
+echo '0' > /dev/stune/rt/schedtune.prefer_idle; chmod 0444 /dev/stune/rt/schedtune.prefer_idle
 #restricted
 echo '0-2,5' > /dev/cpuset/restricted/cpus; chmod 0444 /dev/cpuset/restricted/cpus
 #system background
