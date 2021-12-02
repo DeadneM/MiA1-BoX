afb4994eadda61ffa7c9698d38be8f5e8a9d9ad9
DeadneM <deadnem@gmail.com>
2021-12-02 16:02:40 +0100

    V13 revert and try something old and better

--- Pixel4aBoX/service.sh
+++ Pixel4aBoX/service.sh
@@ -17,23 +17,23 @@ echo '1' > /sys/devices/system/cpu/cpufreq/policy6/schedutil/pl; chmod 0444 /sys
 #general
 echo '0' > /dev/stune/schedtune.boost; chmod 0444 /dev/stune/schedtune.boost
 echo '0' > /dev/stune/schedtune.prefer_high_cap; chmod 0444 /dev/stune/schedtune.prefer_high_cap
-echo '0' > /dev/stune/schedtune.prefer_idle; chmod 0444 /dev/stune/schedtune.prefer_idle
+echo '1' > /dev/stune/schedtune.prefer_idle; chmod 0444 /dev/stune/schedtune.prefer_idle
 echo '0-7' > /dev/cpuset/cpus; chmod 0444 /dev/cpuset/cpus
 #background
 echo '0' > /dev/stune/background/schedtune.boost; chmod 0444 /dev/stune/background/schedtune.boost
 echo '0' > /dev/stune/background/schedtune.prefer_high_cap; chmod 0444 /dev/stune/background/schedtune.prefer_high_cap
 echo '1' > /dev/stune/background/schedtune.prefer_idle; chmod 0444 /dev/stune/background/schedtune.prefer_idle
-echo '0-5' > /dev/cpuset/background/cpus; chmod 0444 /dev/cpuset/background/cpus
+echo '0-2,5' > /dev/cpuset/background/cpus; chmod 0444 /dev/cpuset/background/cpus
 #foreground
 echo '0' > /dev/stune/foreground/schedtune.boost; chmod 0444 /dev/stune/foreground/schedtune.boost
 echo '0' > /dev/stune/foreground/schedtune.prefer_high_cap; chmod 0444 /dev/stune/foreground/schedtune.prefer_high_cap
 echo '1' > /dev/stune/foreground/schedtune.prefer_idle; chmod 0444 /dev/stune/foreground/schedtune.prefer_idle
-echo '0-5' > /dev/cpuset/foreground/cpus; chmod 0444 /dev/cpuset/foreground/cpus
+echo '0-2,5' > /dev/cpuset/foreground/cpus; chmod 0444 /dev/cpuset/foreground/cpus
 #top-app
 echo '0' > /dev/stune/top-app/schedtune.boost; chmod 0444 /dev/stune/top-app/schedtune.boost
-echo '1' > /dev/stune/top-app/schedtune.prefer_high_cap; chmod 0444 /dev/stune/top-app/schedtune.prefer_high_cap
+echo '0' > /dev/stune/top-app/schedtune.prefer_high_cap; chmod 0444 /dev/stune/top-app/schedtune.prefer_high_cap
 echo '1' > /dev/stune/top-app/schedtune.prefer_idle; chmod 0444 /dev/stune/top-app/schedtune.prefer_idle
-echo '0-7' > /dev/cpuset/top-app/cpus; chmod 0444 /dev/cpuset/top-app/cpus
+echo '0-5,7' > /dev/cpuset/top-app/cpus; chmod 0444 /dev/cpuset/top-app/cpus
 #camera-daemon
 echo '0' > /dev/stune/camera-daemon/schedtune.boost; chmod 0444 /dev/stune/camera-daemon/schedtune.boost
 echo '0' > /dev/stune/camera-daemon/schedtune.prefer_high_cap; chmod 0444 /dev/stune/camera-daemon/schedtune.prefer_high_cap
@@ -42,15 +42,15 @@ echo '0-7' > /dev/cpuset/camera-daemon/cpus; chmod 0444 /dev/cpuset/camera-daemo
 #nnapi-hal
 echo '0' > /dev/stune/nnapi-hal/schedtune.boost; chmod 0444 /dev/stune/nnapi-hal/schedtune.boost
 echo '0' > /dev/stune/nnapi-hal/schedtune.prefer_high_cap; chmod 0444 /dev/stune/nnapi-hal/schedtune.prefer_high_cap
-echo '0' > /dev/stune/nnapi-hal/schedtune.prefer_idle; chmod 0444 /dev/stune/nnapi-hal/schedtune.prefer_idle
+echo '1' > /dev/stune/nnapi-hal/schedtune.prefer_idle; chmod 0444 /dev/stune/nnapi-hal/schedtune.prefer_idle
 #rt
 echo '0' > /dev/stune/rt/schedtune.boost; chmod 0444 /dev/stune/rt/schedtune.boost
 echo '0' > /dev/stune/rt/schedtune.prefer_high_cap; chmod 0444 /dev/stune/rt/schedtune.prefer_high_cap
-echo '0' > /dev/stune/rt/schedtune.prefer_idle; chmod 0444 /dev/stune/rt/schedtune.prefer_idle
+echo '1' > /dev/stune/rt/schedtune.prefer_idle; chmod 0444 /dev/stune/rt/schedtune.prefer_idle
 #restricted
-echo '0-5' > /dev/cpuset/restricted/cpus; chmod 0444 /dev/cpuset/restricted/cpus
+echo '0-2,5' > /dev/cpuset/restricted/cpus; chmod 0444 /dev/cpuset/restricted/cpus
 #system background
-echo '0-5' > /dev/cpuset/system-background/cpus; chmod 0444 /dev/cpuset/system-background/cpus
+echo '0-2,5' > /dev/cpuset/system-background/cpus; chmod 0444 /dev/cpuset/system-background/cpus
 # # # CUSTOM SYSTEM UI SETTINGS # # #
 #Disable Left and Right back gestures
 settings put secure back_gesture_inset_scale_left -1; settings put secure back_gesture_inset_scale_right -1
